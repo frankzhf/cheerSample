dc821949f3688638420c3162393533315d673c75
pennyji <1334611717@qq.com>
2017-05-06 15:00:49 +0800

    my first listen demo

---
+++ src/net/frank/cheer/demo/ch01/HelloJava.java
@@ -0,0 +1,17 @@
+package net.frank.cheer.demo.ch01;
+
+public class HelloJava {
+	public static void main(String[] args){
+		System.out.println("Hello　Java");
+		printJavaHome();
+		System.exit(0);
+	}
+	private static void printJavaHome(){
+		System.out.println(System.getProperty("java.home"));
+		System.out.println(System.getProperty("java.home"));
+		System.out.println(System.getProperty("java.home"));
+		System.out.println(System.getProperty("java.home"));
+		
+	}
+	
+}
---
+++ src/net/frank/cheer/exercise/ch01/Subjecct01.java
@@ -0,0 +1,7 @@
+package net.frank.cheer.exercise.ch01;
+
+public class Subjecct01 {
+	public static void main(String[] args[]){
+		System.out.println("This is My first Java program.");
+	}
+}
